ff2f78d09c14c8b81c42f76b57d0ce41fd3867f1
takeshi-R <takeryo.2011@gmail.com>
2020-11-16 22:37:57 +0900

    add use strict to MyPractice22

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+document.getElementById("modalOpen").addEventListener("click", function() {
+  document.getElementById("modal").classList.add("active");
+  document.getElementById("mask").classList.add("active");
+})
+document.getElementById("modalClose").addEventListener("click", function() {
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+})
+document.getElementById("modalOpen01").addEventListener("click", function() {
+  document.getElementById("modal01").classList.add("active");
+  document.getElementById("mask").classList.add("active");
+})
+document.getElementById("modalClose01").addEventListener("click", function() {
+  document.getElementById("modal01").classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+})
