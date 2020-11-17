1ca8d3ad73d7065766dfcdcc2b925e355238445b
takeshi-R <takeryo.2011@gmail.com>
2020-11-17 23:50:41 +0900

    add use strict to script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.getElementById("modalOpen").addEventListener("click", function() {
   document.getElementById("modal").classList.add("active");
   document.getElementById("mask").classList.add("active");
@@ -14,4 +16,3 @@ document.getElementById("modalClose01").addEventListener("click", function() {
   document.getElementById("modal01").classList.remove("active");
   document.getElementById("mask").classList.remove("active");
 })
-
